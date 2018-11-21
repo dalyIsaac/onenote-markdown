6b3a78f9c6f0d375469733e741c3a6f5a33083d8
dalyIsaac <isaac.daly@outlook.com>
2018-11-21 17:13:48 +1300

    Can now insert content at the end of a piece

--- src/page/tree/insert.test.ts
+++ src/page/tree/insert.test.ts
@@ -1,44 +1,82 @@
-// import { OnenotePage } from "@microsoft/microsoft-graph-types";
-// import { createNewPage } from "../createNewPage";
-// import { Color } from "../model";
-// import { SENTINEL_INDEX } from "../reducer";
-// import { IContentInsert, insertContent } from "./insert";
+import { Color, IPageContent } from "../model";
+import { SENTINEL_INDEX } from "../reducer";
+import { createNewPage } from "./createNewPage";
+import { IContentInsert, insertContent } from "./insert";
 
-// describe("page/tree/insert", () => {
-//   test("Insert content at the end of the original content", () => {
-//     const constructOnenotePage = (): OnenotePage => ({
-//       id: "test1",
-//       content:
-//         "Do not go gentle into that good night,\nOld age should burn and rave at close of day;",
-//     });
+describe("page/tree/insert", () => {
+  const constructOriginalPage = (): IPageContent => {
+    const onenotePage = {
+      id: "test1",
+      content:
+        "Do not go gentle into that good night,\nOld age should burn and rave at close of day;",
+    };
+    return createNewPage(onenotePage);
+  };
 
-//     const expectedPage = createNewPage(constructOnenotePage());
-//     expectedPage.buffers.push({
-//       isReadOnly: false,
-//       lineStarts: [0],
-//       content: "\nRage, rage against the dying of the light.",
-//     });
-//     expectedPage.nodes.push({
-//       bufferIndex: 1,
-//       start: { line: 0, column: 0 },
-//       end: { line: 0, column: 43 },
-//       leftCharCount: 0,
-//       leftLineFeedCount: 0,
-//       lineFeedCount: 1,
-//       length: 43,
-//       color: Color.Red,
-//       parent: 0,
-//       left: SENTINEL_INDEX,
-//       right: SENTINEL_INDEX,
-//     });
-//     expectedPage.nodes[0].right = 1;
+  /**
+   * Adds content to the end of the original content.
+   */
+  const constructAddition1 = (): IPageContent => {
+    const page = constructOriginalPage();
+    page.buffers.push({
+      isReadOnly: false,
+      lineStarts: [0, 1],
+      content: "\nRage, rage against the dying of the light.",
+    });
+    page.nodes.push({
+      bufferIndex: 1,
+      start: { line: 0, column: 0 },
+      end: { line: 1, column: 42 },
+      leftCharCount: 0,
+      leftLineFeedCount: 0,
+      lineFeedCount: 1,
+      length: 43,
+      color: Color.Red,
+      parent: 0,
+      left: SENTINEL_INDEX,
+      right: SENTINEL_INDEX,
+    });
+    page.nodes[0].right = 1;
+    return page;
+  };
 
-//     const page = createNewPage(constructOnenotePage());
-//     const insertedContent: IContentInsert = {
-//       content: "\nRage, rage against the dying of the light.",
-//       offset: 84,
-//     };
-//     const acquiredPage = insertContent(insertedContent, page);
-//     expect(acquiredPage).toEqual(expectedPage);
-//   });
-// });
+  /**
+   * Adds content to the end of the original + added content.
+   */
+  const constructAddition2 = (): IPageContent => {
+    const page = constructAddition1();
+    const buffer = page.buffers.pop()!;
+    buffer.content += "\n\nThough wise men at their end know dark is right,";
+    buffer.lineStarts = [...buffer!.lineStarts, 44, 45];
+    page.buffers.push(buffer);
+    const node = page.nodes.pop()!;
+    page.nodes.push({
+      ...node,
+      end: { line: 3, column: 48 },
+      length: 93,
+    });
+    return page;
+  };
+
+  test("Insert content at the end of the original content", () => {
+    const expectedPage = constructAddition1();
+    const page = constructOriginalPage();
+    const insertedContent: IContentInsert = {
+      content: "\nRage, rage against the dying of the light.",
+      offset: 84,
+    };
+    const acquiredPage = insertContent(insertedContent, page);
+    expect(acquiredPage).toEqual(expectedPage);
+  });
+
+  test("Insert content at the end of the content", () => {
+    const expectedPage = constructAddition2();
+    const page = constructAddition1();
+    const insertedContent: IContentInsert = {
+      content: "\n\nThough wise men at their end know dark is right,",
+      offset: 127,
+    };
+    const acquiredPage = insertContent(insertedContent, page);
+    expect(acquiredPage).toEqual(expectedPage);
+  });
+});
--- src/page/tree/insert.ts
+++ src/page/tree/insert.ts
@@ -1,22 +1,131 @@
-// import { IBuffer, IPageContent } from "../model";
-// import { findNodeAtOffset } from "./tree";
+import { CharValues, Color, IBuffer, INode, IPageContent } from "../model";
+import { SENTINEL_INDEX } from "../reducer";
+import {
+  findNodeAtOffset,
+  getLineStarts,
+  INodePosition,
+  MAX_BUFFER_LENGTH,
+} from "./tree";
 
-// export interface IContentInsert {
-//   content: string;
-//   offset: number;
-// }
+export interface IContentInsert {
+  content: string;
+  offset: number;
+}
 
-// export function insertContent(
-//   content: IContentInsert,
-//   pieceTable: IPageContent,
-// ): IPageContent {
-//   const nodes = [...pieceTable.nodes];
-//   const position = findNodeAtOffset(content.offset, nodes, pieceTable.root);
-//   if (position.nodeIndex === Infinity) {
-//     const newBuffer: IBuffer = {
-//       isReadOnly: false,
-//       lineStarts: getLineStarts,
-//     };
-//     const newNode: INode = {};
-//   }
-// }
+/**
+ * Inserts new content into the piece table.
+ * @param newContent Tbe content to insert, and the desired offset.
+ * @param pieceTable The piece table for the OneNote page.
+ */
+export function insertContent(
+  newContent: IContentInsert,
+  pieceTable: IPageContent,
+): IPageContent {
+  let buffers = [...pieceTable.buffers];
+  let nodes = [...pieceTable.nodes];
+  let newPieceTable = { ...pieceTable };
+  const { content, offset } = newContent;
+
+  const position = findNodeAtOffset(offset, nodes, pieceTable.root);
+
+  // insert at the end of a piece
+  if (position.remainder === position.node.length) {
+    ({ buffers, nodes } = insertContentAtEnd(
+      position,
+      content,
+      buffers,
+      nodes,
+      pieceTable.newlineFormat,
+    ));
+  }
+
+  newPieceTable = {
+    ...newPieceTable,
+    buffers,
+    nodes,
+  };
+  // TODO: insert at the start of a piece
+  // TODO: insert inside a piece
+
+  // TODO: fix insert function
+  return newPieceTable;
+}
+
+/**
+ *
+ * @param position The positional information about the offset to insert at.
+ * @param content The new content to insert.
+ * @param buffers Array of the buffers for the piece table.
+ * @param nodes The nodes of the piece table.
+ * @param newlineFormat The newline format.
+ */
+function insertContentAtEnd(
+  position: INodePosition,
+  content: string,
+  buffers: IBuffer[],
+  nodes: INode[],
+  newlineFormat: CharValues[],
+): { buffers: IBuffer[]; nodes: INode[] } {
+  const buffer = buffers[position.node.bufferIndex];
+  if (
+    buffer.content.length + content.length <= MAX_BUFFER_LENGTH &&
+    buffer.isReadOnly === false
+  ) {
+    const newContent = buffer.content + content;
+    // extend the buffer and node
+    const newBuffer: IBuffer = {
+      ...buffer,
+      content: newContent,
+      lineStarts: getLineStarts(newContent, newlineFormat),
+    };
+    buffers[position.node.bufferIndex] = newBuffer;
+    nodes[position.nodeIndex] = {
+      ...position.node,
+      end: {
+        line: newBuffer.lineStarts.length - 1,
+        column:
+          newBuffer.content.length -
+          newBuffer.lineStarts[newBuffer.lineStarts.length - 1],
+      },
+      length: newContent.length,
+    };
+  } else {
+    // create a new buffer and node
+    const newBuffer: IBuffer = {
+      isReadOnly: false,
+      lineStarts: getLineStarts(content, newlineFormat),
+      content,
+    };
+    buffers.push(newBuffer);
+
+    const parentNode: INode = {
+      ...position.node,
+      right: nodes.length,
+    };
+    nodes[position.nodeIndex] = parentNode;
+
+    const newNode: INode = {
+      bufferIndex: buffers.length - 1,
+      start: { line: 0, column: 0 },
+      end: {
+        line: newBuffer.lineStarts.length - 1,
+        column:
+          newBuffer.content.length -
+          newBuffer.lineStarts[newBuffer.lineStarts.length - 1],
+      },
+      leftCharCount: 0,
+      leftLineFeedCount: 0,
+      length: newBuffer.content.length,
+      lineFeedCount: newBuffer.lineStarts.length - 1,
+      color: Color.Red,
+      parent: position.nodeIndex,
+      left: SENTINEL_INDEX,
+      right: SENTINEL_INDEX,
+    };
+    nodes.push(newNode);
+  }
+  return {
+    buffers,
+    nodes,
+  };
+}
