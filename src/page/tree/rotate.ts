bc27aff8418118068eee75c61d5d2098569d94cf
dalyIsaac <isaac.daly@outlook.com>
2018-11-23 13:51:08 +1300

    Modified rotate functions so that they return a new tree. The original tree does not mutate.

--- src/page/tree/rotate.ts
+++ src/page/tree/rotate.ts
@@ -1,12 +1,23 @@
 import { IPageContent } from "../model";
 import { SENTINEL_INDEX } from "../reducer";
 
-export function leftRotate(pieceTable: IPageContent, nodeIndex: number) {
-  const nodes = pieceTable.nodes;
+export function leftRotate(
+  pieceTable: IPageContent,
+  nodeIndex: number,
+): IPageContent {
+  const nodes = [...pieceTable.nodes];
+
   const xIndex = nodeIndex;
-  const x = nodes[xIndex];
+  const x = { ...nodes[xIndex] };
+
+  if (x.right === -1) {
+    //  you can't left rotate
+    return pieceTable;
+  }
   const yIndex = x.right;
-  const y = nodes[yIndex];
+  const y = { ...nodes[yIndex] };
+
+  let root = pieceTable.root;
 
   // fix leftCharCount
   y.leftCharCount += x.leftCharCount + x.length;
@@ -14,30 +25,62 @@ export function leftRotate(pieceTable: IPageContent, nodeIndex: number) {
 
   x.right = y.left;
   if (y.left !== SENTINEL_INDEX) {
-    nodes[y.left].parent = xIndex;
+    nodes[y.left] = {
+      ...nodes[y.left],
+      parent: xIndex,
+    };
   }
   y.parent = x.parent;
   if (x.parent === SENTINEL_INDEX) {
-    pieceTable.root = yIndex;
+    root = yIndex;
   } else if (xIndex === nodes[x.parent].left) {
-    nodes[x.parent].left = yIndex;
+    nodes[x.parent] = {
+      ...nodes[x.parent],
+      left: yIndex,
+    };
   } else {
-    nodes[x.parent].right = yIndex;
+    nodes[x.parent] = {
+      ...nodes[x.parent],
+      right: yIndex,
+    };
   }
   y.left = xIndex;
   x.parent = yIndex;
+
+  nodes[xIndex] = x;
+  nodes[yIndex] = y;
+
+  return {
+    ...pieceTable,
+    nodes,
+    root,
+  };
 }
 
-export function rightRotate(pieceTable: IPageContent, nodeIndex: number) {
-  const nodes = pieceTable.nodes;
+export function rightRotate(
+  pieceTable: IPageContent,
+  nodeIndex: number,
+): IPageContent {
+  const nodes = [...pieceTable.nodes];
+
   const yIndex = nodeIndex;
-  const y = nodes[yIndex];
+  const y = { ...nodes[yIndex] };
+
+  if (y.left === -1) {
+    // you can't right rotate
+    return pieceTable;
+  }
   const xIndex = y.left;
-  const x = nodes[xIndex];
+  const x = { ...nodes[xIndex] };
+
+  let root = pieceTable.root;
 
   y.left = x.right;
   if (x.right !== SENTINEL_INDEX) {
-    nodes[x.right].parent = yIndex;
+    nodes[x.right] = {
+      ...nodes[x.right],
+      parent: yIndex,
+    };
   }
   x.parent = y.parent;
 
@@ -46,12 +89,27 @@ export function rightRotate(pieceTable: IPageContent, nodeIndex: number) {
   y.leftLineFeedCount -= x.leftLineFeedCount + x.lineFeedCount;
 
   if (y.parent === SENTINEL_INDEX) {
-    pieceTable.root = xIndex;
+    root = xIndex;
   } else if (yIndex === nodes[y.parent].right) {
-    nodes[y.parent].right = xIndex;
+    nodes[y.parent] = {
+      ...nodes[y.parent],
+      right: xIndex,
+    };
   } else {
-    nodes[y.parent].left = xIndex;
+    nodes[y.parent] = {
+      ...nodes[y.parent],
+      left: xIndex,
+    };
   }
   x.right = yIndex;
   y.parent = xIndex;
+
+  nodes[yIndex] = y;
+  nodes[xIndex] = x;
+
+  return {
+    ...pieceTable,
+    nodes,
+    root,
+  };
 }
