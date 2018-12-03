3ae5ad40c2f452960b989dab6d40a3c47ad2651b
dalyIsaac <isaac.daly@outlook.com>
2018-12-04 09:23:14 +1300

    Scenario 6 for delete node

--- src/page/tree/delete.test.ts
+++ src/page/tree/delete.test.ts
@@ -4,7 +4,7 @@ import { SENTINEL, SENTINEL_INDEX } from "./tree";
 
 describe("page/tree/delete", () => {
   describe("delete node", () => {
-    test("Simple case", () => {
+    test("Scenario 1: Simple case", () => {
       const page: PageContent = {
         buffers: [],
         previouslyInsertedNodeIndex: null,
@@ -186,7 +186,7 @@ describe("page/tree/delete", () => {
     });
 
     describe("Sibling s is black and at least of one of s's children is red", () => {
-      test("Right right case", () => {
+      test("Scenario 2: Right right case", () => {
         const page: PageContent = {
           buffers: [],
           previouslyInsertedNodeIndex: null,
@@ -401,7 +401,7 @@ describe("page/tree/delete", () => {
         expect(receivedPage).toEqual(expectedPage);
       });
 
-      test("Right left case", () => {
+      test("Scenario 3: Right left case", () => {
         const page: PageContent = {
           buffers: [],
           previouslyInsertedNodeIndex: null,
@@ -580,7 +580,7 @@ describe("page/tree/delete", () => {
         expect(receivedPage).toEqual(expectedPage);
       });
 
-      test("Left left case", () => {
+      test("Scenario 4: Left left case", () => {
         const page: PageContent = {
           buffers: [],
           previouslyInsertedNodeIndex: null,
@@ -795,7 +795,7 @@ describe("page/tree/delete", () => {
         expect(receivedPage).toEqual(expectedPage);
       });
 
-      test("Left right case", () => {
+      test("Scenario 5: Left right case", () => {
         const page: PageContent = {
           buffers: [],
           previouslyInsertedNodeIndex: null,
@@ -971,6 +971,145 @@ describe("page/tree/delete", () => {
         const receivedPage = deleteNode(page, 4);
         expect(receivedPage).toEqual(expectedPage);
       });
+    });
+
+    test("Scenario 6: Sibling s is black, and both its children are black", () => {
+      const page: PageContent = {
+        buffers: [],
+        previouslyInsertedNodeIndex: null,
+        previouslyInsertedNodeOffset: null,
+        newlineFormat: NEWLINE.LF,
+        root: 2,
+        nodes: [
+          SENTINEL,
+          {
+            bufferIndex: 1,
+            start: {
+              line: 0,
+              column: 0,
+            },
+            end: {
+              line: 0,
+              column: 0,
+            },
+            leftCharCount: 0,
+            leftLineFeedCount: 0,
+            length: 10,
+            lineFeedCount: 2,
+            color: Color.Black,
+            parent: 2,
+            left: SENTINEL_INDEX,
+            right: SENTINEL_INDEX,
+          },
+          {
+            bufferIndex: 2,
+            start: {
+              line: 0,
+              column: 0,
+            },
+            end: {
+              line: 0,
+              column: 0,
+            },
+            leftCharCount: 10,
+            leftLineFeedCount: 2,
+            length: 10,
+            lineFeedCount: 2,
+            color: Color.Black,
+            parent: SENTINEL_INDEX,
+            left: 1,
+            right: 3,
+          },
+          {
+            bufferIndex: 3,
+            start: {
+              line: 0,
+              column: 0,
+            },
+            end: {
+              line: 0,
+              column: 0,
+            },
+            leftCharCount: 0,
+            leftLineFeedCount: 0,
+            length: 10,
+            lineFeedCount: 2,
+            color: Color.Black,
+            parent: 2,
+            left: SENTINEL_INDEX,
+            right: SENTINEL_INDEX,
+          },
+        ],
+      };
+      const expectedPage: PageContent = {
+        buffers: [],
+        previouslyInsertedNodeIndex: null,
+        previouslyInsertedNodeOffset: null,
+        newlineFormat: NEWLINE.LF,
+        root: 2,
+        nodes: [
+          SENTINEL,
+          {
+            bufferIndex: 1,
+            start: {
+              line: 0,
+              column: 0,
+            },
+            end: {
+              line: 0,
+              column: 0,
+            },
+            leftCharCount: 0,
+            leftLineFeedCount: 0,
+            length: 10,
+            lineFeedCount: 2,
+            color: Color.Black,
+            parent: SENTINEL_INDEX,
+            left: SENTINEL_INDEX,
+            right: SENTINEL_INDEX,
+          },
+          {
+            bufferIndex: 2,
+            start: {
+              line: 0,
+              column: 0,
+            },
+            end: {
+              line: 0,
+              column: 0,
+            },
+            leftCharCount: 0,
+            leftLineFeedCount: 0,
+            length: 10,
+            lineFeedCount: 2,
+            color: Color.Black,
+            parent: SENTINEL_INDEX,
+            left: SENTINEL_INDEX,
+            right: 3,
+          },
+          {
+            bufferIndex: 3,
+            start: {
+              line: 0,
+              column: 0,
+            },
+            end: {
+              line: 0,
+              column: 0,
+            },
+            leftCharCount: 0,
+            leftLineFeedCount: 0,
+            length: 10,
+            lineFeedCount: 2,
+            color: Color.Red,
+            parent: 2,
+            left: SENTINEL_INDEX,
+            right: SENTINEL_INDEX,
+          },
+        ],
+      };
+      const receivedPage = deleteNode(page, 1);
+      expect(receivedPage).toEqual(expectedPage);
     });
   });
 });
