f2c00313292b90d641befe5436e1640a93b3a779
dalyIsaac <isaac.daly@outlook.com>
2018-12-04 08:42:50 +1300

    Right left case for delete node

--- src/page/tree/delete.test.ts
+++ src/page/tree/delete.test.ts
@@ -400,6 +400,185 @@ describe("page/tree/delete", () => {
         const receivedPage = deleteNode(page, 1);
         expect(receivedPage).toEqual(expectedPage);
       });
+
+      test("Right left case", () => {
+        const page: PageContent = {
+          buffers: [],
+          previouslyInsertedNodeIndex: null,
+          previouslyInsertedNodeOffset: null,
+          newlineFormat: NEWLINE.LF,
+          root: 2,
+          nodes: [
+            SENTINEL,
+            {
+              bufferIndex: 1,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 0,
+              leftLineFeedCount: 0,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Black,
+              parent: 2,
+              left: SENTINEL_INDEX,
+              right: SENTINEL_INDEX,
+            },
+
+            {
+              bufferIndex: 2,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 10,
+              leftLineFeedCount: 2,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Black,
+              parent: SENTINEL_INDEX,
+              left: 1,
+              right: 4,
+            },
+
+            {
+              bufferIndex: 3,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 0,
+              leftLineFeedCount: 0,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Red,
+              parent: 2,
+              left: SENTINEL_INDEX,
+              right: SENTINEL_INDEX,
+            },
+            {
+              bufferIndex: 4,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 10,
+              leftLineFeedCount: 2,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Black,
+              parent: 2,
+              left: 3,
+              right: SENTINEL_INDEX,
+            },
+          ],
+        };
+        const expectedPage: PageContent = {
+          buffers: [],
+          previouslyInsertedNodeIndex: null,
+          previouslyInsertedNodeOffset: null,
+          newlineFormat: NEWLINE.LF,
+          root: 3,
+          nodes: [
+            SENTINEL,
+            {
+              bufferIndex: 1,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 0,
+              leftLineFeedCount: 0,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Black,
+              parent: SENTINEL_INDEX,
+              left: SENTINEL_INDEX,
+              right: SENTINEL_INDEX,
+            },
+            {
+              bufferIndex: 2,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 0,
+              leftLineFeedCount: 0,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Black,
+              parent: 3,
+              left: SENTINEL_INDEX,
+              right: SENTINEL_INDEX,
+            },
+            {
+              bufferIndex: 3,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 10,
+              leftLineFeedCount: 2,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Black,
+              parent: SENTINEL_INDEX,
+              left: 2,
+              right: 4,
+            },
+            {
+              bufferIndex: 4,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 0,
+              leftLineFeedCount: 0,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Black,
+              parent: 3,
+              left: SENTINEL_INDEX,
+              right: SENTINEL_INDEX,
+            },
+          ],
+        };
+        const receivedPage = deleteNode(page, 1);
+        expect(receivedPage).toEqual(expectedPage);
+      });
     });
   });
 });
