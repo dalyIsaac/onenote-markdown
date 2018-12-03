cd0934c8be15eff90fc7b4c2f8d327c1ffc63ace
dalyIsaac <isaac.daly@outlook.com>
2018-12-04 09:05:57 +1300

    Left left case for delete node

--- src/page/tree/delete.test.ts
+++ src/page/tree/delete.test.ts
@@ -579,6 +579,221 @@ describe("page/tree/delete", () => {
         const receivedPage = deleteNode(page, 1);
         expect(receivedPage).toEqual(expectedPage);
       });
+
+      test("Left left case", () => {
+        const page: PageContent = {
+          buffers: [],
+          previouslyInsertedNodeIndex: null,
+          previouslyInsertedNodeOffset: null,
+          newlineFormat: NEWLINE.LF,
+          root: 4,
+          nodes: [
+            SENTINEL,
+            {
+              bufferIndex: 1,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 0,
+              leftLineFeedCount: 0,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Red,
+              parent: 2,
+              left: SENTINEL_INDEX,
+              right: SENTINEL_INDEX,
+            },
+            {
+              bufferIndex: 2,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 10,
+              leftLineFeedCount: 2,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Black,
+              parent: 4,
+              left: 1,
+              right: 3,
+            },
+            {
+              bufferIndex: 3,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 0,
+              leftLineFeedCount: 0,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Red,
+              parent: 2,
+              left: SENTINEL_INDEX,
+              right: SENTINEL_INDEX,
+            },
+            {
+              bufferIndex: 4,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 30,
+              leftLineFeedCount: 6,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Black,
+              parent: SENTINEL_INDEX,
+              left: 2,
+              right: 5,
+            },
+            {
+              bufferIndex: 5,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 0,
+              leftLineFeedCount: 0,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Black,
+              parent: 4,
+              left: SENTINEL_INDEX,
+              right: SENTINEL_INDEX,
+            },
+          ],
+        };
+        const expectedPage: PageContent = {
+          buffers: [],
+          previouslyInsertedNodeIndex: null,
+          previouslyInsertedNodeOffset: null,
+          newlineFormat: NEWLINE.LF,
+          root: 2,
+          nodes: [
+            SENTINEL,
+            {
+              bufferIndex: 1,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 0,
+              leftLineFeedCount: 0,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Black,
+              parent: 2,
+              left: SENTINEL_INDEX,
+              right: SENTINEL_INDEX,
+            },
+            {
+              bufferIndex: 2,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 10,
+              leftLineFeedCount: 2,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Black,
+              parent: SENTINEL_INDEX,
+              left: 1,
+              right: 4,
+            },
+            {
+              bufferIndex: 3,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 0,
+              leftLineFeedCount: 0,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Red,
+              parent: 4,
+              left: SENTINEL_INDEX,
+              right: SENTINEL_INDEX,
+            },
+            {
+              bufferIndex: 4,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 10,
+              leftLineFeedCount: 2,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Black,
+              parent: 2,
+              left: 3,
+              right: SENTINEL_INDEX,
+            },
+            {
+              bufferIndex: 5,
+              start: {
+                line: 0,
+                column: 0,
+              },
+              end: {
+                line: 0,
+                column: 0,
+              },
+              leftCharCount: 0,
+              leftLineFeedCount: 0,
+              length: 10,
+              lineFeedCount: 2,
+              color: Color.Black,
+              parent: SENTINEL_INDEX,
+              left: SENTINEL_INDEX,
+              right: SENTINEL_INDEX,
+            },
+          ],
+        };
+        const receivedPage = deleteNode(page, 5);
+        expect(receivedPage).toEqual(expectedPage);
+      });
     });
   });
 });
