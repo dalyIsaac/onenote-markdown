9238396ce51473fae0fd13fc7404a81529aac2e3
dalyIsaac <isaac.daly@outlook.com>
2018-12-05 17:34:01 +1300

    Scenario 1d test 2 for delete content

--- src/page/tree/delete.test.ts
+++ src/page/tree/delete.test.ts
@@ -3263,7 +3263,7 @@ describe("page/tree/delete", () => {
             {
               bufferIndex: 0,
               start: { line: 1, column: 0 },
-              end: { line: 1, column: 1},
+              end: { line: 1, column: 1 },
               leftCharCount: 2,
               leftLineFeedCount: 0,
               length: 1,
@@ -3280,6 +3280,111 @@ describe("page/tree/delete", () => {
         };
         const receivedPage = deleteContent(page, {
           startOffset: 2,
+          endOffset: 4,
+        });
+        expect(receivedPage).toEqual(expectedPage);
+      });
+
+      test("Scenario 1d: Test 2", () => {
+        const page: PageContent = {
+          buffers: [
+            {
+              content: "abc\ndef",
+              lineStarts: [0, 4],
+              isReadOnly: false,
+            },
+          ],
+          newlineFormat: NEWLINE.LF,
+          nodes: [
+            SENTINEL,
+            {
+              bufferIndex: 0,
+              start: { line: 0, column: 0 },
+              end: { line: 1, column: 1 },
+              leftCharCount: 0,
+              leftLineFeedCount: 0,
+              length: 5,
+              lineFeedCount: 1,
+              color: Color.Black,
+              parent: SENTINEL_INDEX,
+              left: SENTINEL_INDEX,
+              right: 2,
+            },
+            {
+              bufferIndex: 0,
+              start: { line: 1, column: 1 },
+              end: { line: 1, column: 3 },
+              leftCharCount: 0,
+              leftLineFeedCount: 0,
+              length: 2,
+              lineFeedCount: 0,
+              color: Color.Red,
+              parent: 1,
+              left: SENTINEL_INDEX,
+              right: SENTINEL_INDEX,
+            },
+          ],
+          root: 1,
+          previouslyInsertedNodeIndex: 1,
+          previouslyInsertedNodeOffset: 5,
+        };
+        const expectedPage: PageContent = {
+          buffers: [
+            {
+              content: "abc\ndef",
+              lineStarts: [0, 4],
+              isReadOnly: false,
+            },
+          ],
+          newlineFormat: NEWLINE.LF,
+          nodes: [
+            SENTINEL,
+            {
+              bufferIndex: 0,
+              start: { line: 0, column: 0 },
+              end: { line: 0, column: 3 },
+              leftCharCount: 0,
+              leftLineFeedCount: 0,
+              length: 3,
+              lineFeedCount: 0,
+              color: Color.Red,
+              parent: 3,
+              left: SENTINEL_INDEX,
+              right: SENTINEL_INDEX,
+            },
+            {
+              bufferIndex: 0,
+              start: { line: 1, column: 1 },
+              end: { line: 1, column: 3 },
+              leftCharCount: 0,
+              leftLineFeedCount: 0,
+              length: 2,
+              lineFeedCount: 0,
+              color: Color.Red,
+              parent: 3,
+              left: SENTINEL_INDEX,
+              right: SENTINEL_INDEX,
+            },
+            {
+              bufferIndex: 0,
+              start: { line: 1, column: 0 },
+              end: { line: 1, column: 1 },
+              leftCharCount: 3,
+              leftLineFeedCount: 0,
+              length: 1,
+              lineFeedCount: 0,
+              color: Color.Black,
+              parent: SENTINEL_INDEX,
+              left: 1,
+              right: 2,
+            },
+          ],
+          root: 3,
+          previouslyInsertedNodeIndex: null,
+          previouslyInsertedNodeOffset: null,
+        };
+        const receivedPage = deleteContent(page, {
+          startOffset: 3,
           endOffset: 4,
         });
         expect(receivedPage).toEqual(expectedPage);
