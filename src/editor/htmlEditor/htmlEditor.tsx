10de0a2eadd8af40a2be8680734a3567744db99f
dalyIsaac <isaac.daly@outlook.com>
2019-01-11 14:16:09 +1300

    Added selection.

--- src/editor/htmlEditor/htmlEditor.tsx
+++ src/editor/htmlEditor/htmlEditor.tsx
@@ -1,21 +1,57 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Key } from "ts-key-enum";
 import { State } from "../../reducer";
 import { ContentNode, NodeType, PageContent, TagNode } from "../page/model";
 import { areTagEnds, getNodeContent } from "../page/tree/node";
-import { inorderTreeTraversal, SENTINEL } from "../page/tree/tree";
+import { inorderTreeTraversal } from "../page/tree/tree";
 import styles from "./htmlEditor.module.css";
 
+interface SelectionPoint {
+  /**
+   * The DOM element which is involved in the selection.
+   */
+  node: Element;
+
+  /**
+   * The offset of the selection inside the DOM/React element.
+   */
+  offset?: number;
+}
+
+interface Selection {
+  start: SelectionPoint;
+  end?: SelectionPoint;
+}
+
+interface RenderStackItem {
+  node: ContentNode | TagNode;
+  index: number;
+}
+
+interface HtmlEditorProps {
+  page: PageContent;
+}
+
 class HtmlEditorComponent extends React.Component<HtmlEditorProps> {
-  constructor(props: HtmlEditorProps) {
-    super(props);
-  }
+  private static readonly arrowKeys = new Set([
+    Key.ArrowDown,
+    Key.ArrowUp,
+    Key.ArrowLeft,
+    Key.ArrowRight,
+  ]);
+  private static readonly tags = new Set([
+    NodeType.StartTag,
+    NodeType.EndTag,
+    NodeType.StartEndTag,
+  ]);
+
+  private selection: Selection | null = null;
 
   public render(): JSX.Element {
-    let stack: Array<StackItem | JSX.Element> = [];
+    let stack: Array<RenderStackItem | JSX.Element> = [];
     if (this.props.page) {
       for (const { node, index } of inorderTreeTraversal(this.props.page)) {
-        // TODO: use index in getLastStartNode for the search
         if (node.nodeType === NodeType.EndTag) {
           const lastStartNode = this.getLastStartNode(stack);
           if (lastStartNode && areTagEnds(lastStartNode.node, node)) {
@@ -23,11 +59,22 @@ class HtmlEditorComponent extends React.Component<HtmlEditorProps> {
             const elements: Array<string | JSX.Element> = [];
             stack.slice(lastStartNode.index + 1).forEach((x) => {
               if (
-                (x as StackItem).node &&
-                (x as StackItem).node.nodeType === NodeType.Content
+                (x as RenderStackItem).node &&
+                (x as RenderStackItem).node.nodeType === NodeType.Content
               ) {
                 elements.push(
-                  getNodeContent(this.props.page, (x as StackItem).index),
+                  React.createElement(
+                    "span",
+                    {
+                      editornodetype: NodeType.Content,
+                      onPointerDown: this.pointerDown,
+                      onPointerUp: this.pointerUp,
+                    },
+                    getNodeContent(
+                      this.props.page,
+                      (x as RenderStackItem).index,
+                    ),
+                  ),
                 );
               } else {
                 elements.push(x as JSX.Element);
@@ -39,11 +86,16 @@ class HtmlEditorComponent extends React.Component<HtmlEditorProps> {
                 ...lastStartNode.node.properties,
                 style: lastStartNode.node.style,
                 key: lastStartNode.node.id,
+                onPointerDown: this.pointerDown,
+                onPointerUp: this.pointerUp,
+                editornodetype: NodeType.EndTag,
               },
               ...elements,
             );
             stack = stack.slice(0, lastStartNode.index);
             stack.push(newElement);
+          } else {
+            // TODO: there's been some sort of error (mismatch in the stack).
           }
         } else {
           stack.push({ node, index });
@@ -51,17 +103,52 @@ class HtmlEditorComponent extends React.Component<HtmlEditorProps> {
       }
     }
     return (
-      <div className={styles.htmlEditor}>
+      <div
+        className={styles.htmlEditor}
+        contentEditable={true}
+        suppressContentEditableWarning={true}
+        onKeyDown={this.onKeyUp}
+      >
         {this.props.page ? stack : "HTML editor"}
       </div>
     );
   }
 
-  private getLastStartNode(
-    stack: Array<StackItem | JSX.Element>,
-  ): { node: TagNode; index: number } | undefined {
+  private onKeyUp = (e: React.KeyboardEvent): void => {
+    console.log("Key up");
+    if (e.ctrlKey || e.altKey || e.shiftKey || e.metaKey) {
+      console.log("Shortcut");
+      if (HtmlEditorComponent.arrowKeys.has(e.key as Key)) {
+        console.log("Arrow");
+        this.updateSelection(e);
+      }
+    } else {
+      e.preventDefault();
+    }
+  };
+
+  private updateSelection = (e: React.KeyboardEvent): void => {
+    console.log("Update selection");
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    this.selection = {
+      start: {
+        node: range.startContainer as Element,
+        offset: range.startOffset,
+      },
+      end: {
+        node: range.endContainer as Element,
+        offset: range.endOffset,
+      },
+    };
+    console.log(this.selection);
+  };
+
+  private getLastStartNode = (
+    stack: Array<RenderStackItem | JSX.Element>,
+  ): { node: TagNode; index: number } | undefined => {
     for (let i = stack.length - 1; i >= 0; i--) {
-      const node = (stack[i] as StackItem).node;
+      const node = (stack[i] as RenderStackItem).node;
       if (
         node &&
         (node as TagNode).nodeType !== undefined &&
@@ -71,16 +158,57 @@ class HtmlEditorComponent extends React.Component<HtmlEditorProps> {
       }
     }
     return undefined;
-  }
-}
+  };
 
-interface StackItem {
-  node: ContentNode | TagNode;
-  index: number;
-}
+  private pointerDown = (e: React.PointerEvent<HTMLSpanElement>): void => {
+    console.log("Pointer down");
+    const attr = e.currentTarget.attributes.getNamedItem(
+      "editornodetype",
+    ) as Attr;
+    if (attr !== null) {
+      if (attr.value === NodeType.Content) {
+        this.selection = { start: { node: e.currentTarget } };
+      } else if (HtmlEditorComponent.tags.has(attr.value as NodeType)) {
+        // do nothing
+      }
+    }
+    console.log(this.selection);
+  };
 
-interface HtmlEditorProps {
-  page: PageContent;
+  private pointerUp = (e: React.PointerEvent<HTMLSpanElement>): void => {
+    console.log("Pointer up");
+    const attr = e.currentTarget.attributes.getNamedItem(
+      "editornodetype",
+    ) as Attr;
+    const selection = window.getSelection();
+    if (selection.rangeCount > 1) {
+      selection.collapseToStart();
+    }
+    if (attr !== null && this.selection !== null) {
+      if (attr.value === NodeType.Content) {
+        const range = selection.getRangeAt(0);
+        if (selection.anchorNode === selection.focusNode && this.selection) {
+          this.selection.start.offset = range.startOffset;
+          this.selection.end = {
+            node: e.currentTarget,
+            offset: range.endOffset,
+          };
+        } else {
+          this.selection.start.offset = range.startOffset;
+          this.selection.end = {
+            node: e.currentTarget,
+            offset: range.endOffset,
+          };
+        }
+      } else if (HtmlEditorComponent.tags.has(attr.value as NodeType)) {
+        // do nothing
+      }
+    } else {
+      this.selection = null;
+      console.log("Selection is nullified");
+    }
+    console.log(this.selection);
+  };
 }
 
 const mapStateToProps = (state: State): HtmlEditorProps => ({
--- src/editor/toolbar/toolbar.tsx
+++ src/editor/toolbar/toolbar.tsx
@@ -41,16 +41,104 @@ class ToolbarComponent extends React.Component<ToolbarProps, ToolbarState> {
 
 const sampleTextHtml =
   `<html lang="en-NZ">
-<head>
+  <head>
     <title>This is the title</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="created" content="2018-09-03T14:08:00.0000000" />
-</head>
-<body data-absolute-enabled="true" style="font-family:Calibri;font-size:11pt">
-    <p id="p:{6cb59116-8e61-03a9-39ef-edf64004790d}{62}" style="margin-top:0pt;margin-bottom:0pt">` +
-  `<span style="font-weight:bold">Bold</span> text which has <span style="font-style:italic">italics</span> and ` +
-  `<span style="text-decoration:underline">underlines</span></p>
-</body>
+  </head>
+  <body data-absolute-enabled="true" style="font-family:Calibri;font-size:11pt">` +
+  `      <p
+  id="p:{6cb59116-8e61-03a9-39ef-edf64004790d}{62}"
+  style="margin-top:0pt;margin-bottom:0pt"
+>
+  <span style="font-weight:bold">Bold</span> text which has
+  <span style="font-style:italic">italics</span> and
+  <span style="text-decoration:underline">underlines</span>
+</p>
+<p
+  id="p:{a977e62e-3126-8e0d-1368-bcf55df0a6de}{15}"
+  style="margin-top:0pt;margin-bottom:0pt"
+>
+  <span
+    style="font-weight:bold;font-style:italic;text-decoration:underline"
+    >Bold and underlined and italics.
+  </span>
+</p>
+<p
+  id="p:{ee695e3a-1d4a-0a68-043f-8be19ed60633}{11}"
+  style="margin-top:0pt;margin-bottom:0pt"
+>
+  <span style="font-weight:bold;text-decoration:underline"
+    >No italics but bold and underlined</span
+  >
+</p>
+<p
+  id="p:{044b9964-f242-02d2-3bb2-4b6e0d569c68}{11}"
+  style="margin-top:0pt;margin-bottom:0pt"
+>
+  Nothing but
+  <span style="font-weight:bold"
+    >&quot;this is bold and in quotes&quot;</span
+  >
+</p>
+<cite
+  id="cite:{28216e73-1f0a-05fd-25c5-a04844147e70}{16}"
+  style="font-size:9pt;color:#595959;margin-top:0pt;margin-bottom:0pt"
+  >Citation</cite
+>
+<p
+  id="p:{28216e73-1f0a-05fd-25c5-a04844147e70}{19}"
+  style="color:#595959;font-style:italic;margin-top:0pt;margin-bottom:0pt"
+>
+  Quote
+</p>
+<p
+  id="p:{28216e73-1f0a-05fd-25c5-a04844147e70}{21}"
+  style="margin-top:0pt;margin-bottom:0pt"
+>
+  Normal
+</p>
+<p
+  id="p:{28216e73-1f0a-05fd-25c5-a04844147e70}{23}"
+  style="margin-top:0pt;margin-bottom:0pt"
+>
+  <span style="background-color:yellow">Highlighted</span>
+</p>
+<p
+  id="p:{28216e73-1f0a-05fd-25c5-a04844147e70}{26}"
+  style="margin-top:0pt;margin-bottom:0pt"
+>
+  <span style="color:red">Red text</span>
+</p>
+<p
+  id="p:{28216e73-1f0a-05fd-25c5-a04844147e70}{29}"
+  style="margin-top:0pt;margin-bottom:0pt"
+>
+  Superscript x<sup>2</sup>
+</p>
+<p
+  id="p:{28216e73-1f0a-05fd-25c5-a04844147e70}{32}"
+  style="margin-top:0pt;margin-bottom:0pt"
+>
+  Subscript x<sub>2</sub>
+</p>
+<p
+  id="p:{28216e73-1f0a-05fd-25c5-a04844147e70}{35}"
+  style="margin-top:0pt;margin-bottom:0pt"
+>
+  <span style="text-decoration:line-through">Crossed out</span>
+</p>
+<p
+  id="p:{28216e73-1f0a-05fd-25c5-a04844147e70}{38}"
+  data-id="p:{60c45a4d-c9e5-01bd-2ce3-6937733a7742}{11}"
+  style="margin-top:0pt;margin-bottom:0pt"
+>
+  <span style="text-decoration:line-through"
+    >Underlined and crossed out</span
+  >
+</p>
+` +
+  `  </body>
 </html>`;
 
 interface ToolbarProps {
