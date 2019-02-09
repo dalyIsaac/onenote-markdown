7663f866e3cb9b0291cce50e4f03c2bc7b85fd4f
dalyIsaac <isaac.daly@outlook.com>
2019-02-09 14:53:40 +1300

    Switched App.tsx to use a functional component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import Header from "./header/header";
 import MarkdownEditor from "./editor/markdown/markdownEditor";
 import HtmlEditor from "./editor/html/htmlEditor";
 import styles from "./App.module.css";
 
-class App extends Component {
-  public render(): JSX.Element {
-    return (
-      <div className={styles.wrapper}>
-        <Header />
-        <MarkdownEditor />
-        <HtmlEditor />
-      </div>
-    );
-  }
+export default function App(): JSX.Element {
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+      <MarkdownEditor />
+      <HtmlEditor />
+    </div>
+  );
 }
-
-export default App;
