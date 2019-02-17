a98ff2df59eb373fecf49702cdea09757ed1f667
dalyIsaac <isaac.daly@outlook.com>
2019-02-17 18:52:12 +1300

    Can insert a `<br />` right after the previous one.

--- src/page/structureTree/split.test.ts
+++ src/page/structureTree/split.test.ts
@@ -279,6 +279,264 @@ const expectedPage_1_2_2 = (): PageContent => ({
   title: "This is the title",
 });
 
+const expectedPage_1_3_1 = (): PageContent => ({
+  buffers: [
+    {
+      content:
+        "**Bold** text which has _italics_ and {text-decoration:underline}underlines{text-decoration:underline}{!cite} Citation",
+      isReadOnly: true,
+      lineStarts: [0],
+    },
+  ],
+  charset: "utf-8",
+  content: {
+    nodes: [
+      SENTINEL_CONTENT,
+      {
+        bufferIndex: 0,
+        color: Color.Black,
+        end: { column: 118, line: 0 },
+        left: SENTINEL_INDEX,
+        leftCharCount: 0,
+        leftLineFeedCount: 0,
+        length: 118,
+        lineFeedCount: 0,
+        parent: SENTINEL_INDEX,
+        right: SENTINEL_INDEX,
+        start: { column: 0, line: 0 },
+      },
+    ],
+    root: 1,
+  },
+  created: "2018-09-03T14:08:00.0000000",
+  dataAbsoluteEnabled: true,
+  defaultStyle: {
+    fontFamily: "Calibri",
+    fontSize: "11pt",
+  },
+  language: "en-NZ",
+  previouslyInsertedContentNodeIndex: 1,
+  previouslyInsertedContentNodeOffset: 0,
+  structure: {
+    nodes: [
+      SENTINEL_STRUCTURE,
+      {
+        // 1
+        color: Color.Black,
+        id: "p:{6cb59116-8e61-03a9-39ef-edf64004790d}{62}",
+        left: SENTINEL_INDEX,
+        leftSubTreeLength: 0,
+        length: 102,
+        parent: 2,
+        right: SENTINEL_INDEX,
+        style: {
+          marginBottom: "0pt",
+          marginTop: "0pt",
+        },
+        tag: "p",
+        tagType: TagType.StartTag,
+      },
+      {
+        // 2
+        color: Color.Black,
+        id: "p:{6cb59116-8e61-03a9-39ef-edf64004790d}{62}",
+        left: 1,
+        leftSubTreeLength: 1,
+        length: 0,
+        parent: SENTINEL_INDEX,
+        right: 4,
+        tag: "p",
+        tagType: TagType.EndTag,
+      },
+      {
+        // 3
+        color: Color.Black,
+        id: "cite:{28216e73-1f0a-05fd-25c5-a04844147e70}{16}",
+        left: SENTINEL_INDEX,
+        leftSubTreeLength: 0,
+        length: 16,
+        parent: 4,
+        right: SENTINEL_INDEX,
+        style: {
+          color: "#595959",
+          fontSize: "9pt",
+          marginBottom: "0pt",
+          marginTop: "0pt",
+        },
+        tag: "cite",
+        tagType: TagType.StartTag,
+      },
+      {
+        // 4
+        color: Color.Red,
+        id: "cite:{28216e73-1f0a-05fd-25c5-a04844147e70}{16}",
+        left: 3,
+        leftSubTreeLength: 1,
+        length: 0,
+        parent: 2,
+        right: 5,
+        tag: "cite",
+        tagType: TagType.EndTag,
+      },
+      {
+        // 5
+        color: Color.Black,
+        id: "{!localGeneratedId}1234567890",
+        left: SENTINEL_INDEX,
+        leftSubTreeLength: 0,
+        length: 0,
+        parent: 4,
+        right: SENTINEL_INDEX,
+        tag: "br",
+        tagType: TagType.StartEndTag,
+      },
+      {
+        // 6
+        color: Color.Black,
+        id: "{!localGeneratedId}1234567890",
+        left: SENTINEL_INDEX,
+        leftSubTreeLength: 0,
+        length: 0,
+        parent: SENTINEL_INDEX,
+        right: SENTINEL_INDEX,
+        tag: "p",
+        tagType: TagType.EndTag,
+      },
+    ],
+    root: 2,
+  },
+  title: "This is the title",
+});
+
+const expectedPage_1_3_2 = (): PageContent => ({
+  buffers: [
+    {
+      content:
+        "**Bold** text which has _italics_ and {text-decoration:underline}underlines{text-decoration:underline}{!cite} Citation",
+      isReadOnly: true,
+      lineStarts: [0],
+    },
+  ],
+  charset: "utf-8",
+  content: {
+    nodes: [
+      SENTINEL_CONTENT,
+      {
+        bufferIndex: 0,
+        color: Color.Black,
+        end: { column: 118, line: 0 },
+        left: SENTINEL_INDEX,
+        leftCharCount: 0,
+        leftLineFeedCount: 0,
+        length: 118,
+        lineFeedCount: 0,
+        parent: SENTINEL_INDEX,
+        right: SENTINEL_INDEX,
+        start: { column: 0, line: 0 },
+      },
+    ],
+    root: 1,
+  },
+  created: "2018-09-03T14:08:00.0000000",
+  dataAbsoluteEnabled: true,
+  defaultStyle: {
+    fontFamily: "Calibri",
+    fontSize: "11pt",
+  },
+  language: "en-NZ",
+  previouslyInsertedContentNodeIndex: 1,
+  previouslyInsertedContentNodeOffset: 0,
+  structure: {
+    nodes: [
+      SENTINEL_STRUCTURE,
+      {
+        // 1
+        color: Color.Black,
+        id: "p:{6cb59116-8e61-03a9-39ef-edf64004790d}{62}",
+        left: SENTINEL_INDEX,
+        leftSubTreeLength: 0,
+        length: 102,
+        parent: 2,
+        right: SENTINEL_INDEX,
+        style: {
+          marginBottom: "0pt",
+          marginTop: "0pt",
+        },
+        tag: "p",
+        tagType: TagType.StartTag,
+      },
+      {
+        // 2
+        color: Color.Black,
+        id: "p:{6cb59116-8e61-03a9-39ef-edf64004790d}{62}",
+        left: 1,
+        leftSubTreeLength: 1,
+        length: 0,
+        parent: SENTINEL_INDEX,
+        right: 3,
+        tag: "p",
+        tagType: TagType.EndTag,
+      },
+      {
+        // 3
+        color: Color.Red,
+        id: "cite:{28216e73-1f0a-05fd-25c5-a04844147e70}{16}",
+        left: 5,
+        leftSubTreeLength: 1,
+        length: 16,
+        parent: 2,
+        right: 4,
+        style: {
+          color: "#595959",
+          fontSize: "9pt",
+          marginBottom: "0pt",
+          marginTop: "0pt",
+        },
+        tag: "cite",
+        tagType: TagType.StartTag,
+      },
+      {
+        // 4
+        color: Color.Black,
+        id: "cite:{28216e73-1f0a-05fd-25c5-a04844147e70}{16}",
+        left: SENTINEL_INDEX,
+        leftSubTreeLength: 0,
+        length: 0,
+        parent: 3,
+        right: SENTINEL_INDEX,
+        tag: "cite",
+        tagType: TagType.EndTag,
+      },
+      {
+        // 5
+        color: Color.Black,
+        id: "{!localGeneratedId}1234567890",
+        left: SENTINEL_INDEX,
+        leftSubTreeLength: 0,
+        length: 0,
+        parent: 3,
+        right: SENTINEL_INDEX,
+        tag: "br",
+        tagType: TagType.StartEndTag,
+      },
+      {
+        // 6
+        color: Color.Black,
+        id: "{!localGeneratedId}1234567890",
+        left: SENTINEL_INDEX,
+        leftSubTreeLength: 0,
+        length: 0,
+        parent: SENTINEL_INDEX,
+        right: SENTINEL_INDEX,
+        tag: "p",
+        tagType: TagType.EndTag,
+      },
+    ],
+    root: 2,
+  },
+  title: "This is the title",
+});
+
 describe("Tests for splitting `StructureNode`s.", () => {
   test("1.1.1 Split the final `StructureNode`.", () => {
     const page = parse(test_06_html);
@@ -598,6 +856,32 @@ describe("Tests for splitting `StructureNode`s.", () => {
 
   test("1.3.1 Converts the empty `StructureNode` into a `<br />` tag, and removes the paired end tag.", () => {
     const page = expectedPage_1_2_1();
+    const action: SplitStructureAction = {
+      localContentOffset: 0,
+      nodeContentOffset: 118,
+      nodeIndex: 5,
+      pageId: "",
+      type: SPLIT_STRUCTURE_NODE,
+    };
+    splitStructureNode(page as PageContentMutable, action);
+    expect(page).toStrictEqual(expectedPage_1_3_1());
+  });
+
+  test("1.3.2 Converts the empty `StructureNode` into a `<br />` tag, and removes the paired end tag.", () => {
+    const page = expectedPage_1_2_2();
+    const action: SplitStructureAction = {
+      localContentOffset: 0,
+      nodeContentOffset: 102,
+      nodeIndex: 5,
+      pageId: "",
+      type: SPLIT_STRUCTURE_NODE,
+    };
+    splitStructureNode(page as PageContentMutable, action);
+    expect(page).toStrictEqual(expectedPage_1_3_2());
+  });
+
+  test("1.4.1 Inserts an empty `<br />` tag after the existing `<br />` tag", () => {
+    const page = expectedPage_1_3_1();
     const expectedPage: PageContent = {
       buffers: [
         {
@@ -705,11 +989,7 @@ describe("Tests for splitting `StructureNode`s.", () => {
             leftSubTreeLength: 0,
             length: 0,
             parent: 4,
-            right: SENTINEL_INDEX,
-            style: {
-              marginBottom: "0pt",
-              marginTop: "0pt",
-            },
+            right: 7,
             tag: "br",
             tagType: TagType.StartEndTag,
           },
@@ -724,6 +1004,18 @@ describe("Tests for splitting `StructureNode`s.", () => {
             right: SENTINEL_INDEX,
             tag: "p",
             tagType: TagType.EndTag,
+          },
+          {
+            // 7
+            color: Color.Red,
+            id: "{!localGeneratedId}1234567890",
+            left: SENTINEL_INDEX,
+            leftSubTreeLength: 0,
+            length: 0,
+            parent: 5,
+            right: SENTINEL_INDEX,
+            tag: "br",
+            tagType: TagType.StartEndTag,
           },
         ],
         root: 2,
@@ -741,8 +1033,8 @@ describe("Tests for splitting `StructureNode`s.", () => {
     expect(page).toStrictEqual(expectedPage);
   });
 
-  test("1.3.2 Converts the empty `StructureNode` into a `<br />` tag, and removes the paired end tag.", () => {
-    const page = expectedPage_1_2_2();
+  test("1.4.2 Inserts an empty `<br />` tag after the existing `<br />` tag", () => {
+    const page = expectedPage_1_3_2();
     const expectedPage: PageContent = {
       buffers: [
         {
@@ -850,11 +1142,7 @@ describe("Tests for splitting `StructureNode`s.", () => {
             leftSubTreeLength: 0,
             length: 0,
             parent: 3,
-            right: SENTINEL_INDEX,
-            style: {
-              marginBottom: "0pt",
-              marginTop: "0pt",
-            },
+            right: 7,
             tag: "br",
             tagType: TagType.StartEndTag,
           },
@@ -869,6 +1157,18 @@ describe("Tests for splitting `StructureNode`s.", () => {
             right: SENTINEL_INDEX,
             tag: "p",
             tagType: TagType.EndTag,
+          },
+          {
+            // 7
+            color: Color.Red,
+            id: "{!localGeneratedId}1234567890",
+            left: SENTINEL_INDEX,
+            leftSubTreeLength: 0,
+            length: 0,
+            parent: 5,
+            right: SENTINEL_INDEX,
+            tag: "br",
+            tagType: TagType.StartEndTag,
           },
         ],
         root: 2,
--- src/page/structureTree/split.ts
+++ src/page/structureTree/split.ts
@@ -1,7 +1,7 @@
 import { PageContentMutable, Color } from "../pageModel";
 import { SplitStructureAction } from "./actions";
 import { findEndTag, generateNewId } from "./tree";
-import { StructureNodeMutable, TagType } from "./structureModel";
+import { StructureNodeMutable, TagType, StructureNode } from "./structureModel";
 import { SENTINEL_INDEX } from "../tree/tree";
 import { insertNode, fixInsert } from "../tree/insert";
 import { deleteStructureNode } from "./delete";
@@ -53,16 +53,45 @@ function splitPopulatedNode(
   fixInsert(page.structure, page.structure.nodes.length - 1);
 }
 
+function insertNewBreak(
+  page: PageContentMutable,
+  nodeContentOffset: number,
+  previousBreakIndex: number,
+): void {
+  const newNode: StructureNode = {
+    color: Color.Red,
+    id: generateNewId(),
+    left: SENTINEL_INDEX,
+    leftSubTreeLength: 0,
+    length: 0,
+    parent: SENTINEL_INDEX,
+    right: SENTINEL_INDEX,
+    tag: "br",
+    tagType: TagType.StartEndTag,
+  };
+  insertNode(page.structure, newNode, nodeContentOffset, previousBreakIndex);
+}
+
+/**
+ * Converts the empty `StructureNode` into a `<br />` tag, and removes the
+ * paired end tag.
+ */
 function makeEmptyBreak(
   page: PageContentMutable,
+  startNodeIndex: number,
   startNode: StructureNodeMutable,
   endIndex: number,
 ): void {
-  startNode.tag = "br";
-  startNode.tagType = TagType.StartEndTag;
+  const { attributes, style, ...newStartNode } = startNode;
+  newStartNode.tag = "br";
+  newStartNode.tagType = TagType.StartEndTag;
+  page.structure.nodes[startNodeIndex] = newStartNode;
   deleteStructureNode(page, endIndex);
 }
 
+/**
+ * Splits a `StructureNode` if the node is populated, or inserts a `<br />` tag.
+ */
 export function splitStructureNode(
   page: PageContentMutable,
   {
@@ -74,6 +103,10 @@ export function splitStructureNode(
   const startNode: StructureNodeMutable = {
     ...page.structure.nodes[startNodeIndex],
   };
+  if (startNode.tag === "br" && startNode.tagType === TagType.StartEndTag) {
+    insertNewBreak(page, nodeContentOffset, startNodeIndex);
+    return;
+  }
   const endTag = findEndTag(page.structure, startNode.id, startNodeIndex);
   if (endTag === null) {
     throw new RangeError(
@@ -92,7 +125,6 @@ export function splitStructureNode(
       endIndex,
     );
   } else {
-    // TODO: check if the node is a <br /> tag
-    makeEmptyBreak(page, startNode, endIndex);
+    makeEmptyBreak(page, startNodeIndex, startNode, endIndex);
   }
 }
